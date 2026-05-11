4c9e7091bd06dd3053d9b60f00c6b5f25cfad34d
Artur Bieniek <ar2rekb@gmail.com>
2026-05-11 22:43:08 +0200

    Add timeout

--- pwn/poni barn/solution/run.sh
+++ pwn/poni barn/solution/run.sh
@@ -4,6 +4,6 @@ EXPLOIT_FILE=$(pwd)/exploit.in
 
 pushd ../challenge >/dev/null
 
-cat "$EXPLOIT_FILE" | ./run.sh | awk -F'|' '/^VAL:/ { printf "%s", $2; fflush() }'
+cat "$EXPLOIT_FILE" | timeout 5s ./run.sh | awk -F'|' '/^VAL:/ { printf "%s", $2; fflush() }'
 
 popd
